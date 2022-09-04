46b3fc7442ded383946f3ec1d24f796f66965323
Vinícius Gonçalves <103079267+gonssalves@users.noreply.github.com>
2022-09-03 22:12:30 -0300

    Create 3764.py
    Acidez de várias soluções

---
+++ 3764.py
@@ -0,0 +1,11 @@
+ph = float(input(""))
+
+while ph != -1:
+   if ph < 7:
+       print("ACIDA")
+   elif ph > 7:
+       print("BASICA")
+   else:
+       print("NEUTRA")
+
+   ph = float(input(""))
